2503505e3ae28bb0c8cef7681840d50cc7f3a134
Seth Jernigan <SethJernigan8@outlook.com>
2023-07-20 19:42:01 -0600

    First commit new tutorial

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
-from website import create_app
+""" from website import create_app
 
 app = create_app()
 
 if __name__ == '__main__': # only runs if you run this file directly
+    app.run(debug=True) """
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return "hello, world"
+
+
+if __name__ == "__main__":
     app.run(debug=True)
 

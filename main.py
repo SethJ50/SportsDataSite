3a9dc1e8b2732b9834dc8a9610fa7ba53be5c017
Seth Jernigan <SethJernigan8@outlook.com>
2023-07-20 20:29:17 -0600

    Added static, home template, css, html

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ app = create_app()
 if __name__ == '__main__': # only runs if you run this file directly
     app.run(debug=True) """
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "hello, world"
+    return render_template('home.html')
 
 
 if __name__ == "__main__":

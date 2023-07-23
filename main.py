ede691a3cdb1e56617c9f9d36acffd18aaf06c94
Seth Jernigan <SethJernigan8@outlook.com>
2023-07-23 13:04:31 -0600

    Added Dynamic Data

--- main.py
+++ main.py
@@ -5,14 +5,44 @@ app = create_app()
 if __name__ == '__main__': # only runs if you run this file directly
     app.run(debug=True) """
 
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS = [
+    {
+        'id': 1,
+        'title': 'Data Analyst',
+        'location': 'Bengaluru, India',
+        'salary': '$250,000'
+    }, 
+    {
+        'id': 2,
+        'title': 'Data Scientist',
+        'location': 'Los Angeles, USA',
+        'salary': '$300,000'
+    },
+    {
+        'id': 3,
+        'title': 'Front End Engineer',
+        'location': 'Remote',
+        'salary': '$100,000'
+    },
+    {
+        'id': 4,
+        'title': 'Back End Engineer',
+        'location': 'Tucson, USA',
+        'salary': '$120,000'
+    }
+]
+
 @app.route('/')
 def hello_world():
-    return render_template('home.html')
+    return render_template('home.html', jobs=JOBS)
 
+@app.route('/api/jobs')
+def list_jobs():
+    return jsonify(JOBS)
 
 if __name__ == "__main__":
     app.run(debug=True)

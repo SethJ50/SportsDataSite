9a4f69fc00096418010765c7e6eea75f976220b6
Seth Jernigan <SethJernigan8@outlook.com>
2023-07-23 18:19:45 -0600

    Added database initial

---
+++ database.py
@@ -0,0 +1,19 @@
+import sqlalchemy
+from sqlalchemy import create_engine, text
+import os
+
+db_connection_string = str(os.environ.get('DB_CONNECTION_STRING'))
+
+engine = create_engine(db_connection_string)
+
+
+def load_jobs_from_db():
+    with engine.connect() as conn:
+        result = conn.execute(text("select * from jobs"))
+        jobs = []
+        for row in result.all():
+            jobs.append(row._asdict())
+    return jobs
+
+
+print(load_jobs_from_db())
--- main.py
+++ main.py
@@ -6,43 +6,20 @@ if __name__ == '__main__': # only runs if you run this file directly
     app.run(debug=True) """
 
 from flask import Flask, render_template, jsonify
+from database import load_jobs_from_db
 
 app = Flask(__name__)
 
-JOBS = [
-    {
-        'id': 1,
-        'title': 'Data Analyst',
-        'location': 'Bengaluru, India',
-        'salary': '$250,000'
-    }, 
-    {
-        'id': 2,
-        'title': 'Data Scientist',
-        'location': 'Los Angeles, USA',
-        'salary': '$300,000'
-    },
-    {
-        'id': 3,
-        'title': 'Front End Engineer',
-        'location': 'Remote',
-        'salary': '$100,000'
-    },
-    {
-        'id': 4,
-        'title': 'Back End Engineer',
-        'location': 'Tucson, USA',
-        'salary': '$120,000'
-    }
-]
 
 @app.route('/')
 def hello_world():
-    return render_template('home.html', jobs=JOBS)
+    jobs = load_jobs_from_db()
+    return render_template('home.html', jobs=jobs)
 
 @app.route('/api/jobs')
 def list_jobs():
-    return jsonify(JOBS)
+    jobs = load_jobs_from_db()
+    return jsonify(jobs)
 
 if __name__ == "__main__":
     app.run(debug=True)

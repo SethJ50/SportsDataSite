570c884b2687ff28180e4a6ef5a220faff293381
Seth Jernigan <SethJernigan8@outlook.com>
2023-07-23 21:57:24 -0600

    changed db to cloud for trial

--- database.py
+++ database.py
@@ -18,7 +18,8 @@ def load_jobs_from_db():
 
 pwd = str(os.environ.get('mySQLConnectPwd'))
 
-connection = mysql.connector.connect(user='root', database='sportsdatadb', password=pwd, host='localhost')
+#connection = mysql.connector.connect(user='root', database='sportsdatadb', password=pwd, host='localhost')
+connection = mysql.connector.connect(user='e68amx23ko5ytmll3z2z', database='sportsdb2', password='pscale_pw_U7nPPiBk7XA62RqFUgHaaKey1jJR6k4Ire2AwXYVqtL', host='aws.connect.psdb.cloud')
 
 def load_jobs_from_db():
     if connection.is_connected():
@@ -31,3 +32,5 @@ def load_jobs_from_db():
         for row in cursor:
             jobs.append(row)
     return jobs
+
+print(load_jobs_from_db())

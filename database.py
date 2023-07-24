de251deb417290431a96806258bf8e11abbd6ed2
Seth Jernigan <SethJernigan8@outlook.com>
2023-07-23 19:36:43 -0600

    change to mysql.connector

--- database.py
+++ database.py
@@ -1,8 +1,9 @@
 import sqlalchemy
 from sqlalchemy import create_engine, text
 import os
+import mysql.connector
 
-db_connection_string = str(os.environ.get('DB_CONNECTION_STRING'))
+""" db_connection_string = str(os.environ.get('DB_CONNECTION_STRING'))
 
 engine = create_engine(db_connection_string)
 
@@ -13,4 +14,20 @@ def load_jobs_from_db():
         jobs = []
         for row in result.all():
             jobs.append(row._asdict())
+    return jobs """
+
+pwd = str(os.environ.get('mySQLConnectPwd'))
+
+connection = mysql.connector.connect(user='root', database='sportsdatadb', password=pwd, host='localhost')
+
+def load_jobs_from_db():
+    if connection.is_connected():
+        db_Info = connection.get_server_info()
+        print("Connected to MySQL Server version ", db_Info)
+        cursor = connection.cursor(dictionary=True)
+
+        cursor.execute("SELECT * FROM jobs")
+        jobs = []
+        for row in cursor:
+            jobs.append(row)
     return jobs
